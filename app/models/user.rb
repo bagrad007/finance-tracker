8400490557454ef053856a064e3ee629e6a132d1
Luke Gill <bagrad007@gmail.com>
2024-10-24 14:29:29 -0400

    creates button and logic to add stock to portfolio

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,22 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
-  validates_presence_of :name, :ticker, on: :create, message: "can't be blank"
   # validates_uniqueness_of :name, :ticker, on: :create, message: "has already been taken"
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+
+  def under_stock_limit?
+    (stocks.count < 10) ? true : false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks
+  resources :user_stocks, only: [:create]
   devise_for :users
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
