e1188fd17fab0e6b195b68600331c85b2fb267ed
Luke Gill <bagrad007@gmail.com>
2024-10-24 15:08:28 -0400

    adds ability to remove stock from portfolio

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -8,9 +8,19 @@ class UserStocksController < ApplicationController
     end
 
     @user_stock = UserStock.create(user: current_user, stock: stock)
-    
-    flash[:success] = "Stock Added to Portfolio"
+
+    flash[:notice] = "#{stock.name} Added to Portfolio"
+    redirect_to my_portfolio_path
+  end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+
+    flash[:notice] = "#{stock.name} was removed from portfolio."
     redirect_to my_portfolio_path
   end
   
+
 end
